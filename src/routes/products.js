7dd37dd7955666b9283336080d4da27e24d2023f
GazdagB <gazdagbal@gmail.com>
2025-04-17 22:01:37 +0200

    ✨ Adds get product endpoint

--- src/routes/products.js
+++ src/routes/products.js
@@ -12,10 +12,31 @@ router.get('/', async (req, res) => {
         `)
         res.json(result.rows)
     } catch (error) {
-        console.error("Error running query: ", err)
+        console.error("Error running query: ", error)
         res.status(500).send('Database error');
     }
 });
+
+router.get("/:id", async (req,res)=>{
+    try{
+        const result = await db.query(`
+            SELECT p.id, p.name, p.description, p.price, p.image_url, c.name AS category_name 
+            FROM products p
+            JOIN categories c ON p.category_id =  c.id
+            WHERE p.id = $1
+            `, [req.params.id])
+
+
+
+            if(result.rows.length === 0){
+                return res.status(404).json({message: "Prdocut not found"})
+            }
+        res.json(result.rows[0]); 
+    } catch(error){
+        console.log("Error running query: ", error); 
+        res.status(500).send('Database error'); 
+    }
+})
 
 router.post("/", async (req, res) => {
     try {
