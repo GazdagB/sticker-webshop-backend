f24e368ba437f4d576ec3c4357ff4f8094fc1bd7
GazdagB <gazdagbal@gmail.com>
2025-04-28 22:24:29 +0200

    ✨ Adding basic crud endpoints for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser, getAllUsers } from '../services/usersService.js';
+import { createUser, getAllUsers, getUserById, deleteUser,softDeleteUser, updateUser } from '../services/usersService.js';
 import { userValidationRules } from '../validators/userValidator.js';
 import { validate } from '../middlewares/validate.js';
 import bcrypt from 'bcrypt';
@@ -52,5 +52,64 @@ router.post('/', userValidationRules, validate, async (req,res) =>{
         res.status(500).send('Database error');
     }
 })
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await getUserById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error('Error running query: ', error);
+        res.status(500).send('Database error');
+    }
+});
+
+//TODO: Add validation for updateUser
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { first_name, last_name, username, email } = req.body;
+    try {
+        const updatedUser = await updateUser(id, { first_name, last_name, username, email });
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User updated successfully", user: updatedUser });
+    } catch (error) {
+        console.error('Error running query: ', error);
+        res.status(500).send('Database error');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedUser = await deleteUser(id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User deleted successfully" });
+    } catch (error) {
+        console.error('Error running query: ', error);
+        res.status(500).send('Database error');
+    }
+});
+
+router.patch('/:id/delete', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedUser = await softDeleteUser(id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User deleted successfully" });
+    } catch (error) {
+        console.error('Error running query: ', error);
+        res.status(500).send('Database error');
+    }
+});
+
 
 export default router;
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -2,7 +2,7 @@ import db from "../db/index.js";
 
 export async function getAllUsers() {
     const result = await db.query(`
-        SELECT last_name, first_name, email 
+        SELECT id, last_name, first_name, email 
         FROM users 
         WHERE is_deleted = false
     `);
@@ -15,6 +15,46 @@ export async function createUser(user) {
         `INSERT INTO users (username, first_name, last_name, email, password_hash) 
          VALUES ($1, $2, $3, $4, $5) RETURNING id, username, first_name, last_name, email`,
         [user.username, user.first_name, user.last_name, user.email, user.password]
+    );
+    return result.rows[0];
+}
+
+export async function getUserById(id) {
+    const result = await db.query(
+        `SELECT id, username, first_name, last_name, email 
+         FROM users 
+         WHERE id = $1 AND is_deleted = false`,
+        [id]
+    );
+    return result.rows[0];
+}
+
+export async function updateUser(id, user) {
+    const result = await db.query(
+        `UPDATE users 
+         SET username = $1, first_name = $2, last_name = $3, email = $4 
+         WHERE id = $5 RETURNING id, username, first_name, last_name, email`,
+        [user.username, user.first_name, user.last_name, user.email, id]
+    );
+    return result.rows[0];
+}
+
+export async function deleteUser(id) {
+    const result = await db.query(
+        `UPDATE users 
+         SET is_deleted = true 
+         WHERE id = $1 RETURNING id`,
+        [id]
+    );
+    return result.rows[0];
+}
+
+export async function softDeleteUser(id) {
+    const result = await db.query(
+        `UPDATE users 
+         SET is_deleted = true 
+         WHERE id = $1 RETURNING id`,
+        [id]
     );
     return result.rows[0];
 }
