9373067cd087dd17d08345d2c9066c89d84a5df2
GazdagB <gazdagbal@gmail.com>
2025-05-03 23:29:00 +0200

    🛂 Adds a login endpoint

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import productsRouter from './routes/products.js';
 import categoryRouter from "./routes/categories.js"
 import usersRouter from "./routes/users.js";
+import loginRouter from "./routes/login.js";
 
 const app = express(); 
 
@@ -12,6 +13,7 @@ const PORT = process.env.APP_PORT || 8080;
 app.use("/products", productsRouter);
 app.use("/categories", categoryRouter )
 app.use("/users", usersRouter);
+app.use("/login", loginRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Listening at port: ${PORT}`);
